a5bd1a4a584578bd9b608a5cf440f42541634fdc
pbaile64 <pbaile64@ford.com>
2018-12-23 12:45:47 -0500

    Passing test for Item name and regularPrice KATA-001

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -2,11 +2,20 @@ import java.math.BigDecimal;
 
 public class Item {
 
+    private String name;
+    private BigDecimal regularPrice;
+
     public Item(String name, BigDecimal regularPrice){
+        this.name = name;
+        this.regularPrice = regularPrice;
     }
 
     public String getName(){
-        return "";
+        return this.name;
+    }
+
+    public BigDecimal getRegularPrice(){
+        return this.regularPrice;
     }
 
 }
--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -10,8 +10,10 @@ public class ItemTest {
     @Test
     public void shouldSetItemNameAndRegularPriceOnInstantiation(){
         String expectedName = "milk";
-        Item milk = new Item(expectedName, new BigDecimal("2.00"));
+        BigDecimal expectedRegularPrice = new BigDecimal("2.00");
+        Item milk = new Item(expectedName, expectedRegularPrice);
         assertThat(milk.getName(), is(expectedName));
+        assertThat(milk.getRegularPrice(), is(expectedRegularPrice));
     }
 
 }
