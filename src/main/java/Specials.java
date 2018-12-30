29c60dc9fc0c38c59c1113917411cb5872942c7f
pbaile64 <pbaile64@ford.com>
2018-12-30 13:45:40 -0500

    Added minimum and percent off reg price for Specials. KATA-004

--- src/main/java/Specials.java
+++ src/main/java/Specials.java
@@ -1,15 +1,27 @@
 public enum Specials {
 
-    BUY_ONE_GET_ONE_FREE("4");
+    BUY_ONE_GET_ONE_FREE("2", "4", Percentage.FIFTY_PERCENT.getPercentageValueString());
 
     private String itemLimit;
+    private String itemMinimum;
+    private String percentOffRegPrice;
 
-    private Specials(String itemLimit){
+    private Specials(String itemMinimum, String itemLimit, String percentOffRegPrice){
+        this.itemMinimum = itemMinimum;
         this.itemLimit = itemLimit;
+        this.percentOffRegPrice = percentOffRegPrice;
     }
 
     public String getItemLimit(){
         return this.itemLimit;
+    }
+
+    public String getItemMinimum(){
+        return this.itemMinimum;
+    }
+
+    public String getPercentOffRegPrice(){
+        return this.percentOffRegPrice;
     }
 
 }
